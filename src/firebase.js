f4a15df34151e022ba5e3fa53e4620ba14a1f56c
Yu Chen Liu <vincent6786@users.noreply.github.com>
2026-02-23 20:18:58 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123...",
+  apiKey: "AIzaSyC-Tqgi3N2FQDva_1GRsNLduwd1zG0zrdc",
+  authDomain: "crewlog-890ba.firebaseapp.com",
+  projectId: "crewlog-890ba",
+  storageBucket: "crewlog-890ba.firebasestorage.app",
+  messagingSenderId: "220639273214",
+  appId: "1:220639273214:web:d02d5318ecd7bc51d485af"
 };
 
 const app = initializeApp(firebaseConfig);
